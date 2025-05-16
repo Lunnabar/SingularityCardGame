5b8871b7bb11547cd9bb4d12316b6c4301403ec1
J W <jjw4@williams.edu>
2025-05-16 10:36:14 -0400

    Added Deck

--- SCG/Deck.java
+++ SCG/Deck.java
@@ -4,20 +4,37 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.awt.geom.Rectangle2D;
 
 // Creating decks
 
-public class Deck<T> extends ArrayList<T>{
 
-// init
-	public Deck(){
-		super(); 
-	}
+public class Deck extends Rectangle2D.Double {
+	private ArrayList<Card> deck;
+	private int index;
+	private int width;
 
+// Init
+	public Deck(int index, int width) {
+    	super(index * width, 0, width, width);
+    	deck = new ArrayList<Card>();
+        this.index = index;
+        this.width = width;
+    }
 
-	// Shuffles the cards
-	public void Shuffle(){
-		Random random = new Random();
-		
-	}
+    public boolean isEmpty() {
+        return deck.size() != 0;
+    }
+
+    public void setCard(Card play) {
+        deck.add(play);
+    }
+
+    public Card release() {
+    	Random rand = new Random();
+    	int index = rand.nextInt(deck.size());
+        Card play = deck.get(index);
+        deck.remove(index);
+        return play;
+    }
 }
