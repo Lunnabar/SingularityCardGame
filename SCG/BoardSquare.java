bbab77e78899883009d3bbfc455175fb860c682f
J W <jjw4@williams.edu>
2025-05-19 00:01:49 -0400

    Finished BoardSquare

---
+++ SCG/BoardSquare.java
@@ -0,0 +1,43 @@
+package SCG; 
+
+import java.util.ArrayList;
+import java.awt.geom.Rectangle2D;
+
+// Creating each squares on the board
+
+public class BoardSquare extends Rectangle2D.Double {
+	private Card card;
+	private BitboardADT index;
+	private int width;
+
+// Init
+	public CardSquare(int index, int width) {
+    	super((index%8) * width, (index/8)*width, width, width);
+    	card = null;
+        this.index = index;
+        this.width = width;
+    }
+
+// Checks for card in square
+    public boolean isOccupied() {
+        return card != null;
+    }
+
+// Sets card in square
+    public void setCard(Card play) {
+        card = play;
+        card.moveTo((index%8)*width+ width/4,(index/8)*width+ width*5/6, width/2, width*2/3);
+    }
+
+// Removes card from square
+    public Card release() {
+        Card play = card;
+        card = null;
+        return play;
+    }
+
+// Tells what card is in the square
+    public Card getCard() {
+        return card;
+    }
+}
