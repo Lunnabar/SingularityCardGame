7ecbf44cbf34eadcec0451e5900d3c917c5edd95
J W <jjw4@williams.edu>
2025-05-18 23:09:56 -0400

    Finished Card

--- SCG/Card.java
+++ SCG/Card.java
@@ -5,22 +5,20 @@ import javax.swing.JFrame;
 public class Card extends Rectangle2D.Double{
 
 	private JFrame board;
-	private int diameter;
+	private int width;
 
-	public Card(JFrame Gameboard, int diam) {
-		super(diam / 2, diam / 2, diam, diam * 1.414);
+	public Card(JFrame Gameboard, int width) {
+		super(width/4, width*5/6, width/2, width*2/3);
         board = Gameboard;
-        diameter = diam;
     }
 
-    public Card(JFrame Gameboard, int diam, int xCoord, int yCoord) {
-        super(xCoord - diam / 2, yCoord - diam / 2, diam, diam * 1.414);
+    public Card(JFrame Gameboard, int width, int row, int column) {
+        super(column*width+ width/4,row*width+ width*5/6, width/2, width*2/3);
         board = Gameboard;
-        diameter = diam;
     }
 
-    public void moveTo(int xCoord, int yCoord) {
-        setFrame(xCoord - diameter / 2, yCoord - diameter / 2, diameter, diameter * 1.414);
+    public void moveTo(int row, int column) {
+        setFrame(column*width+ width/4,row*width+ width*5/6, width/2, width*2/3);
         board.repaint();
     }
 }
